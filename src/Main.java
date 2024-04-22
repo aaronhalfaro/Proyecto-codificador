7974484c8870e769f613587093ef2e7ab2b25fc5
Aarón Alfaro <aarohnx@gmail.com>
2024-04-21 20:06:34 -0600

    Se añade la clase Encriptador

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,37 @@
-import java.util.Locale;
 import java.util.Scanner;
-
 public class Main {
-    private static void ejecutaSeleccion(String s) throws InterruptedException {
-        if (s.equals("1") || s.equals("encriptar")){
 
-            System.out.println("se está encriptando tu archivo...");
-            Thread.sleep(1000);
-            System.out.println("Archivo encriptado con éxito");
-            Thread.sleep(500);
+    private static void ejecutaSeleccion(String seleccion){
+        Scanner console = new Scanner(System.in);
+        switch (seleccion) {
+            case "1", "encriptar" -> {
+                System.out.print("Ingrese la clave de codificación para cifrado cesar: ");
+                int clave = console.nextInt();
 
-        } else if (s.equals("2") || s.equals("desencriptar")) {
+                System.out.print("Ingrese el nombre del archivo que desea encriptar: ");
 
-            System.out.println("se está desencriptando tu archivo...");
-            Thread.sleep(1000);
-            System.out.println("Archivo desencriptado con éxito");
-            Thread.sleep(500);
-        }else if (s.equals("0") || s.equals("salir")) {
-            System.out.println("Hasta pronto!");
-            return;
-        }else{
-            System.out.println("Opción invalida, intente de nuevo por favor.");
+                String archivoParaEncriptar = new Scanner(System.in).nextLine();
+
+                System.out.println(archivoParaEncriptar);
+
+                System.out.println("se está encriptando el archivo " + archivoParaEncriptar + "...");
+
+                Encriptador.encriptarArchivo(clave, archivoParaEncriptar);
+
+                System.out.println("Archivo encriptado con éxito");
+            }
+            case "2", "desencriptar" -> {
+
+                System.out.println("se está desencriptando tu archivo...");
+//                Thread.sleep(1000);
+                System.out.println("Archivo desencriptado con éxito");
+//                Thread.sleep(500);
+            }
+            case "0", "salir" -> {
+                System.out.println("Hasta pronto!");
+                return;
+            }
+            default -> System.out.println("Opción invalida, intente de nuevo por favor.");
         }
         System.out.println("Pulsa \"enter\" para regresar al menú.");
         new Scanner(System.in).nextLine();
@@ -28,18 +39,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         String seleccion;
+        System.out.println("Programa que encripta un archivo .txt");
         do {
-            System.out.println("1.- Encriptar\n" +
-                                "2.- Desencriptar\n" +
-                                "0.- salir\n");
+            System.out.println("""
+                        1.- Encriptar
+                        2.- Desencriptar
+                        0.- salir
+                    """);
             System.out.print("Elige una opción: ");
             seleccion = console.nextLine().toLowerCase();
 
-            try {
-                ejecutaSeleccion(seleccion);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+            ejecutaSeleccion(seleccion);
 
 
         }while(!(seleccion.equals("0") || seleccion.equals("salir")));
